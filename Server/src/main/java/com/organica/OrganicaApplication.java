f469f04053570f6a484b2f138b99d368cf67d0ef
vivek <vivek.kakadiya111@gmail.com>
2023-05-20 18:33:17 +0530

    First

---
+++ Server/src/main/java/com/organica/OrganicaApplication.java
@@ -0,0 +1,13 @@
+package com.organica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicaApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/organica/OrganicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
